df5437c52911fb28ac659668fba0740797d5e3b3
alex <109907113+alex-faryna@users.noreply.github.com>
2023-02-25 17:46:20 +0200

    table with borders + load additional rows

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React, {ReactNode, useState} from 'react';
 import './App.css';
 
 type ColumnTemplate<T> = (value: unknown, key: keyof T, ...args: unknown[]) => ReactNode;
@@ -10,106 +10,110 @@ interface Column<T extends ColumnData> {
     body?: ColumnTemplate<T>,
 }
 
-function Table<T extends ColumnData>({ columns, data }: { columns: Column<T>[], data: T[] }) {
-    const createRow = (row: T) => columns.map(({ body, key }) =>
-        <td key={key}>{ body ? body(row[key], key, row) : `${row[key]}` }</td>);
-    const body = data.map(row => <tr key={row.id}>{ createRow(row) }</tr>);
-    const header = columns.map(({ header, key }) => <th key={key}>{ header }</th>);
+function Table<T extends ColumnData>({
+                                         columns,
+                                         data,
+                                         loadMore,
+                                         threshold = 10.0
+                                     }: { columns: Column<T>[], data: T[], loadMore: Function, threshold?: number }) {
+    const createRow = (row: T) => columns.map(({body, key}) =>
+        <td key={key}>{body ? body(row[key], key, row) : `${row[key]}`}</td>);
+    const body = data.map(row => <tr key={row.id}>{createRow(row)}</tr>);
+    const header = columns.map(({header, key}) => <th key={key}>{header}</th>);
 
-    return <div className='table-container'>
+    const scroll = (event: React.UIEvent) => {
+        const element = event.target as HTMLElement;
+        const scrolled = Math.abs(element.scrollHeight - element.scrollTop - element.clientHeight) <= threshold;
+        if (scrolled) {
+            loadMore();
+        }
+    }
+
+    return <div className='table-container' onScroll={scroll}>
         <table>
             <thead>
             <tr>
-                { header }
+                {header}
             </tr>
             </thead>
             <tbody>
-            { body }
+            {body}
             </tbody>
+            <tfoot>
+            <tr>
+                <td colSpan={columns.length || 1}>footer</td>
+            </tr>
+            </tfoot>
         </table>
     </div>
 }
 
 
 function App() {
+    const arr = [
+        {
+            id: 0,
+            key1: 'val1',
+            key2: 56
+        },
+        {
+            id: 1,
+            key1: 'val 45',
+            key2: 34
+        },
+        {
+            id: 10,
+            key1: 'val1',
+            key2: 56
+        },
+        {
+            id: 11,
+            key1: 'val 45',
+            key2: 34
+        },
+        {
+            id: 20,
+            key1: 'val1',
+            key2: 56
+        },
+        {
+            id: 21,
+            key1: 'val 45',
+            key2: 34
+        },
+        {
+            id: 30,
+            key1: 'val1',
+            key2: 56
+        },
+        {
+            id: 31,
+            key1: 'val 45',
+            key2: 34
+        },
+        {
+            id: 40,
+            key1: 'val1',
+            key2: 56
+        },
+        {
+            id: 41,
+            key1: 'val 45',
+            key2: 34
+        },
+        {
+            id: 50,
+            key1: 'val1',
+            key2: 56
+        },
+        {
+            id: 51,
+            key1: 'val 45',
+            key2: 34
+        },
+    ];
 
-    const data = [
-        {
-            id: 0,
-            key1: 'val1',
-            key2: 56
-        },
-        {
-            id: 1,
-            key1: 'val 45',
-            key2: 34
-        },
-        {
-            id: 0,
-            key1: 'val1',
-            key2: 56
-        },
-        {
-            id: 1,
-            key1: 'val 45',
-            key2: 34
-        },
-        {
-            id: 0,
-            key1: 'val1',
-            key2: 56
-        },
-        {
-            id: 1,
-            key1: 'val 45',
-            key2: 34
-        },{
-            id: 0,
-            key1: 'val1',
-            key2: 56
-        },
-        {
-            id: 1,
-            key1: 'val 45',
-            key2: 34
-        },{
-            id: 0,
-            key1: 'val1',
-            key2: 56
-        },
-        {
-            id: 1,
-            key1: 'val 45',
-            key2: 34
-        },{
-            id: 0,
-            key1: 'val1',
-            key2: 56
-        },
-        {
-            id: 1,
-            key1: 'val 45',
-            key2: 34
-        },{
-            id: 0,
-            key1: 'val1',
-            key2: 56
-        },
-        {
-            id: 1,
-            key1: 'val 45',
-            key2: 34
-        },{
-            id: 0,
-            key1: 'val1',
-            key2: 56
-        },
-        {
-            id: 1,
-            key1: 'val 45',
-            key2: 34
-        },
-    ]
+    const [data, setData] = useState(arr);
 
     const columns: Column<typeof data[0]>[] = [
         {
@@ -120,13 +124,17 @@ function App() {
             header: 'header 2',
             key: 'key2',
             body: (value: any, key: string, row: any) => {
-                return <><h1>{ value }</h1><span>{ row['key1'] }</span></>;
+                return <><h1>{value}</h1><span>{row['key1']}</span></>;
             },
         }
     ];
 
+    const load = () => {
+        setData([...data, ...data].map((row, id) => ({...row, id})));
+    }
+
     return <div className='container'>
-        <Table columns={columns} data={data}></Table>
+        <Table columns={columns} data={data} loadMore={load}></Table>
     </div>
 }
 
